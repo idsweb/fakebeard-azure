63a15594a9a76b0371cacc0ac686afa05f893fc7
Ian Stafford <iandstafford@googlemail.com>
2019-06-28 07:16:56 +0100

    Added new scripts

---
+++ docs/readme.md
@@ -0,0 +1,4 @@
+#Azure 70-533 Notes
+
+This repo contains my notes on videos and other material around the Azure exams
+
---
+++ scripts/storage.ps1
@@ -0,0 +1,17 @@
+﻿# Get-AzureRmResourceGroup | select ResourceGroupName
+# Remove-AzureRmResourceGroup -Name binit -Force
+# New-AzureRmResourceGroup -Name ids-strg -Location 'West Europe'
+
+<# Set the name of the storage account and the SKU name. 
+$storageAccountName = "idsstg"
+$skuName = "Standard_LRS"
+
+# Create the storage account.
+$storageAccount = New-AzureRmStorageAccount -ResourceGroupName ids-strg `
+  -Name $storageAccountName `
+  -Location 'West Europe' `
+  -SkuName $skuName #>
+
+$stacc = Get-AzureRmStorageAccount -Name 'idsstg' -ResourceGroupName ids-strg
+
+Get-AzureRmStorageAccountKey -ResourceGroupName ids-strg -Name 'idsstg'
